ec00293830b3491e775056cf105c427361a01f81
Mona Lisa <Mona@.com>
2024-11-11 20:45:06 -0500

    add no recipe is found

--- recipe-search-app/src/App.js
+++ recipe-search-app/src/App.js
@@ -5,13 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipeList from './components/RecipeList';
 function App() {
   const [recipes, setRecipes] = useState([]); // Manage fetched recipes state
-  
+  const handleClick = () => {
+    window.location.href = '/new-page'; // Replace '/new-page' with your desired path
+};
   return (
     <div className="App">
       <SearchBar setRecipes={setRecipes} />
       {/* Display the fetched recipes here */}
       
       <RecipeList recipes={recipes}/>
+    <button onClick={handleClick}>Redirect to New Page</button>
+
     </div>
   );
 }
--- recipe-search-app/src/components/RecipeList.js
+++ recipe-search-app/src/components/RecipeList.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 
 const RecipeList = ({ recipes }) => {
+  const message = !recipes && "No recipe is found";
+  const [selected, setSelectedIndex] =useState(-1);
+  
   return (
     <div>
+      {message}
       {recipes && recipes.length > 0 && (
         <ul className='list-group'>
           {recipes.map((recipe, index) => (
             <li
-              className="list-group-item"
+              className={selected === index ? "list-group-item active" : "list-group-item"}
               key={index} // Use index as a unique key
+              onClick={()=>
+              {
+                setSelectedIndex(index);
+              }
+              }
             >
               {recipe.strMeal} - {recipe.strArea}
             </li>
